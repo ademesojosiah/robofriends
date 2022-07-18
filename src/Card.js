fa3236c488daea996682824ba571ce9908c8f8ca
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-18 18:19:13 +0100

    created a card.js component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ()=>{
+    return(
+        <div>
+            <img src='' alt=''></img>
+            <div>
+                <h1>welcome</h1>
+                <p>see</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
