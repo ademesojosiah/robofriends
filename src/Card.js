e8f2d9e31731f3f41320092dee9f1bdaa052bd5f
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-19 23:44:51 +0100

    install tachyons dependency

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ()=>{
+const Card = (props)=>{
     return(
-        <div>
-            <img src='' alt=''></img>
+        <div className='bg-light-green dib tc br3 pa3 ma2 grow bw2 shadow-5' >
+            <img src={`https://robohash.org/${props.id}?200x200`} alt='robot'></img>
             <div>
-                <h1>welcome</h1>
-                <p>see</p>
+                <h1>{props.name}</h1>
+                <p>{props.email}</p>
             </div>
         </div>
     )
