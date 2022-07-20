50d16eefb8a6cb0b90619c35abd508062361dcf8
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-20 23:41:30 +0100

    completed and updated the search input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 
-const App = ()=>{
-    return(
-    <div className='tc'>
-        <h1>Robofriends</h1>
-        <SearchBox/>
-        <Cardlist robots={robots}/>
-    </div>
-    )
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots:robots,
+            searchField:''
+        }
+    }
+
+
+    onSearch=(e)=>{
+        this.setState({searchField:e.target.value})
+
+    }
+
+
+
+    render(){
+        const filteredArray = this.state.robots.filter((robot)=>{
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return(
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox unSearch={this.onSearch}/>
+                <Cardlist robots={filteredArray}/>
+            </div>
+        )
+    }
 }
 
 export default App;
