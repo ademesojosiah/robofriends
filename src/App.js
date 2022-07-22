390f9124b3618bf01331a4903e0a5dbafbbdd362
jojo114 <68749841+jojo114@users.noreply.github.com>
2022-07-23 00:02:11 +0100

    created a state in the app(parent class) so the children can interact with each other

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React,{Component}  from 'react';
 import Cardlist from './Cardlist';
 import SearchBox from './SearchBox'
-import {robots} from './robots';
+// import {robots} from './robots';
+import './App.css'
 
 class App extends Component{
     constructor(){
         super()
-        this.state = {
-            robots:robots,
-            searchField:''
+        this.state ={
+            robots:[],
+            searchField: ''
         }
+
     }
 
 
-    onSearch=(e)=>{
-        this.setState({searchField:e.target.value})
+    componentDidMount(){
+        fetch('jsonplaceholder.typicode.com/users')
+            .then((response)=>{response.json()})
+            .then((users)=>{this.setState({robots:users})});
+    }
 
+    onsearch=(e)=>{
+        this.setState({searchField:e.target.value})
     }
 
 
@@ -24,14 +31,14 @@ class App extends Component{
         const filteredArray = this.state.robots.filter((robot)=>{
             return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
-        return(
-            <div className='tc'>
-                <h1>Robofriends</h1>
-                <SearchBox unSearch={this.onSearch}/>
-                <Cardlist robots={filteredArray}/>
-            </div>
-        )
-    }
-}
 
+    return(
+    <div className='tc'>
+        <h1 className='hhs f1'>Robofriends</h1>
+        <SearchBox unSearch={this.onsearch}/>
+        <Cardlist robots={filteredArray}/>
+    </div>
+    );
+}
+}
 export default App;
